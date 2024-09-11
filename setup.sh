cc368e1c32392fc70c79cd066e61d9e8eaf63056
NadiaHajighassem <ucloud@j-5079456-job-0.j-5079456.ucloud-apps.svc.cluster.local>
2024-09-11 15:04:20 +0200

    adding setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+git config --global user.name "NadiaHajighassem"
+git config --global user.email "hajighassemnadia@gmail.com" # adding git credentials :)
+# to run make sure you are in the correct folder in terminal and write bash setup.sh
+
