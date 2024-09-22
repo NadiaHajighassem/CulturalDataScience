56a964321489a70e447d6f329f78c333abb16576
NadiaHajighassem <hajighassemnadia@gmail.com>
2024-09-22 12:25:13 +0200

    setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 git config --global user.name "NadiaHajighassem"
 git config --global user.email "hajighassemnadia@gmail.com" # adding git credentials :)
 # to run make sure you are in the correct folder in terminal and write bash setup.sh
